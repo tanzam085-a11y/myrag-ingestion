1c00dacdefa595a4ba280d51689b2a179b2b074f
tanzam085-a11y <tanzam085@gmail.com>
2026-09-03 23:37:19 -0700

    Add files via upload

---
+++ .env.py
@@ -0,0 +1,4 @@
+Python 3.13.15 (tags/v3.13.15:4061bc4, Aug  5 2026, 13:05:39) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> GOOGLE_API_KEY=
+... QDRANT_URL=https://3192041d-57d6-40bd-a134-de547156afa5.us-west-1-0.aws.cloud.qdrant.io
